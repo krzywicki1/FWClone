58617c90d0a96555cb0c005803d673b23d29bfc6
ukasz <lukaszkrz@protonmail.com>
2023-02-02 22:09:00 +0100

    moviesList

--- src/main/java/com/example/p2/viewmodel/MoviesVM.java
+++ src/main/java/com/example/p2/viewmodel/MoviesVM.java
@@ -12,6 +12,7 @@ import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zk.ui.util.Notification;
 import org.zkoss.zkplus.spring.DelegatingVariableResolver;
+import org.zkoss.zul.ListModelList;
 
 import static com.ns.util.En.A1;
 
@@ -21,24 +22,32 @@ public class MoviesVM {
     @WireVariable
     private MovieRepository movieRepository;
 
-    private Movies movies;
-    public Movies getMovies(){return movies;}
+    private ListModelList<Movies> movies;
+    private Movies newMovie;
+    public ListModelList<Movies> getMovies() { return movies; }
 
-    public void setMovies(Movies movies) {this.movies = movies; }
+    public void setNewMovie(Movies newMovie) { this.newMovie = newMovie; }
+    public void setMovies(ListModelList<Movies> movies) { this.movies = movies; }
 
     public @Init void doInit(@ContextParam(ContextType.SESSION) Session session) {
         log.debug("movies doInit() called");
-        setMovies(new Movies());
+        setNewMovie(new Movies());
+        setMovies(new ListModelList<>(movieRepository.findAll()));
+
         // Initialize data
     }
 
     //Wywoływanie metod
-    public static final String CMDX = "cmd1";
+    public static final String CMDX = "cmd1", CMD_SHOW_MOVIES = "cmd2";
 
     @Command(CMDX)
     public void cmd1()
     {
         log.debug("movies test");
+    }
+
+    @Command(CMD_SHOW_MOVIES)
+    public void cmd2(){
 
     }
 }
